b4fff7ffbd3acff1b71f00cf7a14976ea1074607
kazekim <jirawat.h@kazekim.com>
2019-06-30 14:54:33 +0700

    - add grpc test - add Unary service

---
+++ greet_client/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-go-course/greetpb"
+	"log"
+)
+
+func main() {
+
+	fmt.Println("Hello, I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Couldn't connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	//fmt.Printf("Created client: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Start to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Jirawat",
+			LastName: "Harnsiriwatanakit",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Println("Response from Greet: ", res.Result)
+}
---
+++ greet_server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"grpc-go-course/greetpb"
+	"log"
+	"net"
+)
+
+type server struct{}
+
+func (*server) Greet(c context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error){
+
+	fmt.Println("Greet function was invoked with %v", req)
+	firstName := req.GetGreeting().GetFirstName()
+	lastName := req.GetGreeting().GetLastName()
+	result := "Hello, " + firstName + " " + lastName
+	res := &greetpb.GreetResponse{
+		Result:result,
+	}
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
